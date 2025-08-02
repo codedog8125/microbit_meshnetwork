b9bc340e9e9cd9dfb8c21db7b6455d0b964322e2
codedog8125 <160143804+codedog8125@users.noreply.github.com>
2025-08-02 13:50:39 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
+import radio, datalogger
 
+# === system setup ===
+
+#sets up the radio
+radio.set_group(27)
+radio.set_transmit_power(7)
+
+# shows that it is ready
+basic.show_icon(IconNames.YES)
+# === varibele setup ===
+
+# gives the node config to be use later and to check info
+node_conifg = { 
+    "node_ID" : "SN-1", 
+    "node_firmware" : "1-beta",
+    "node_enabled" : True
+}
+
+# sets up the network conifg to pull data form later
+network_config = {
+    "node_ID" : node_conifg["node_ID"],
+    "SN_network_IDs" : ["SN-1", "SN-2"],
+    "UN_network_IDs" : ["UN-1", "UN-2"]
+}
+
+
+message_data = {
+    "msg_type" : 1,
+    "UN_recevied_list" :["UN-2"],
+    "SN_recevied_list" : ["SN-2"],
+    "hop_count" : 2
+}
+
+# === funtion setup ===
+# makes the message ready for use to be sent or receved
+def prase(string):
+    return "testdata"
+def stringify(data):
+    pass
+
+def on_received_string(receivedString):
+    
+    pass 
+
+
+# === the code start ===
+def on_forever():
+    if node_conifg["node_enabled"] == True:
+        radio.on_received_string(on_received_string)
+    else:
+        basic.show_leds("""
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        """)
+
+
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,55 @@
+//  === system setup ===
+// sets up the radio
+radio.setGroup(27)
+radio.setTransmitPower(7)
+//  shows that it is ready
+basic.showIcon(IconNames.Yes)
+//  === varibele setup ===
+//  gives the node config to be use later and to check info
+let node_conifg = {
+    "node_ID" : "SN-1",
+    "node_firmware" : "1-beta",
+    "node_enabled" : true,
+}
 
+//  sets up the network conifg to pull data form later
+let network_config = {
+    "node_ID" : node_conifg["node_ID"],
+    "SN_network_IDs" : ["SN-1", "SN-2"],
+    "UN_network_IDs" : ["UN-1", "UN-2"],
+}
+
+let message_data = {
+    "msg_type" : 1,
+    "UN_recevied_list" : ["UN-2"],
+    "SN_recevied_list" : ["SN-2"],
+    "hop_count" : 2,
+}
+
+//  === funtion setup ===
+//  makes the message ready for use to be sent or receved
+function prase(string: any): string {
+    return "testdata"
+}
+
+function stringify(data: any) {
+    
+}
+
+//  === the code start ===
+basic.forever(function on_forever() {
+    if (node_conifg["node_enabled"] == true) {
+        radio.onReceivedString(function on_received_string(receivedString: string) {
+            
+        })
+    } else {
+        basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    }
+    
+})
